ca9bcb1bb52e7ffd021fe33d9d499119faf703eb
Matej Bačo <matejbaco2000@gmail.com>
2025-05-19 03:56:01 +0200

    syntax fix

--- src/KubernetesCluster.php
+++ src/KubernetesCluster.php
@@ -274,7 +274,7 @@ class KubernetesCluster
             $separator = \strpos($chunk, "\n");
             if ($separator !== false) {
                 $extra = \substr($chunk, $separator + \strlen("\n"));
-                $chunk = \substr($chunk, 0, $separator) + "\n";
+                $chunk = \substr($chunk, 0, $separator) . "\n";
 
                 $call = call_user_func($callback, $chunk);
                 if (!is_null($call)) {
