36d64390b3295be60744a3dc9b7b578fd8f6a89d
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-04-02 10:41:25 +0530

    Add container restart policy accessors

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -2,6 +2,7 @@
 
 namespace RenokiCo\PhpK8s\Test;
 
+use RenokiCo\PhpK8s\Enums\RestartPolicy;
 use RenokiCo\PhpK8s\Instances\MountedVolume;
 use RenokiCo\PhpK8s\Instances\Probe;
 use RenokiCo\PhpK8s\K8s;
@@ -21,6 +22,7 @@ class ContainerTest extends TestCase
             ->addConfigMapRefs(['SECRET_TWO' => ['cm_ref_name', 'cm_ref_key']])
             ->addFieldRefs(['NODE_NAME' => ['spec.nodeName']])
             ->setArgs(['--test'])
+            ->setRestartPolicy(RestartPolicy::ALWAYS)
             ->addPort(80, 'TCP', 'http')
             ->addPort(443, 'TCP', 'https')
             ->setMountedVolumes([$volume->mountTo('/some/path')]);
@@ -98,6 +100,7 @@ class ContainerTest extends TestCase
         $this->assertStringEndsWith('nginx:1.23', $container->getImage());
         $this->assertEquals([], $container->getEnv([]));
         $this->assertEquals(['--test'], $container->getArgs());
+        $this->assertEquals(RestartPolicy::ALWAYS, $container->getRestartPolicy());
         $this->assertEquals([
             ['name' => 'http', 'protocol' => 'TCP', 'containerPort' => 80],
             ['name' => 'https', 'protocol' => 'TCP', 'containerPort' => 443],
